4430410b6f680f422e78d21be948430cd7de6ca8
Iron <ironxxx@yandex.ru>
2026-03-17 13:55:04 +0300

    test5

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('Hello from repository!')
+# Импорт load_dotenv.
+from dotenv import load_dotenv 
+
+# Импорт библиотеки для работы с окружением.
+import os  
+
+# Загрузка переменных из .env
+load_dotenv()
+
+# Получение значения переменной author из .env
+author = os.getenv('AUTHOR')
+
+def print_author(author):
+# Допишите здесь ваш код
+    print(f"Автор проекта: {author}") 
+
+
+print_author (author)
